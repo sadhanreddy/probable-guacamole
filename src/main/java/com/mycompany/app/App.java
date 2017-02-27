3955d83bcfb5fd2d170883ccc18b8462f27c2ad9
sailaja <sailajavijayk@gmail.com>
2017-02-27 04:47:21 +0000

    change

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Bolo Zor se Hello World!I am changing again HURRAY" );
+        System.out.println( "Hello World!I am changing again HURRAY" );
 
     }
 }
