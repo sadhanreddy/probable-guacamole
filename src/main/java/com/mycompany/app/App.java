be54a46c4b58ff65adc65bf8e16be744f26685ca
sailaja <sailajavijay@k.com>
2017-03-08 02:15:40 +0000

    change java file

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "  GIT HOOK trigger build" );
+        System.out.println( "   HOOK trigger build" );
 
     }
 }
