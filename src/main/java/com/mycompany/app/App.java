e2667a02fc2a18d5257163caf75bb136847091f8
sailaja <sailajavijay@k.com>
2017-03-08 02:34:07 +0000

    change new

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( " now SCM   trigger build" );
+        System.out.println( " now SCM   rigger build" );
 
     }
 }
