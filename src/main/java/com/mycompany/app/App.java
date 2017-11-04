4940805a5b77e4c72b5f16636ad7f655e5696a93
root <root@ip-10-0-5-76.ec2.internal>
2017-11-04 07:29:56 +0000

    chnge java file

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( " Hurray to automation !!! SCM   trigger build" );
+        System.out.println( "Welcome to the Workshop !!! SCM   trigger build" );
 
 
 
